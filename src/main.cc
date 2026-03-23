b858d5b0cfdca3fdf8c4f10bc6e892b10953c93b
LinF-PDX <fulin200028@gmail.com>
2026-03-23 17:54:35 -0400

    Add macro for readability

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,18 @@
 #include "simulator.hpp"
 #include "controller.hpp"
 
+#define SIMULATION_TOTAL_TIME_SECOND 10.0
+#define SIMULATION_TIME_STEP 0.0001
+#define SIMULATION_LOG_INTERVAL 0.01
+#define ROBOT_LINK1_LENGTH 1.0
+#define ROBOT_LINK2_LENGTH 1.0
+#define ROBOT_LINK1_MASS 1.0
+#define ROBOT_LINK2_MASS 1.0
+
 int main() {
-    RobotModel robot(1.0, 1.0, 1.0, 1.0);
-    Simulator sim(0.0001);
-    const double log_interval = 0.01;
+    RobotModel robot(ROBOT_LINK1_LENGTH, ROBOT_LINK2_LENGTH, ROBOT_LINK1_MASS, ROBOT_LINK2_MASS);
+    Simulator sim(SIMULATION_TIME_STEP);
+    const double log_interval = SIMULATION_LOG_INTERVAL;
 
     RobotState state;
     state.q << -M_PI/6, 0.0;
@@ -31,7 +39,7 @@ int main() {
     logger.writeRow({state.time, q1_deg, q2_deg, q1dot_deg, q2dot_deg, xy(0), xy(1), tau(0), tau(1)});
     next_log_time += log_interval;
 
-    for (int i = 0; i < 100000; ++i) {
+    for (int i = 0; i < SIMULATION_TOTAL_TIME_SECOND / SIMULATION_TIME_STEP; ++i) {
         sim.stepSimulation(robot, tau, state);
         xy = robot.forwardKinematics(state.q);
 
